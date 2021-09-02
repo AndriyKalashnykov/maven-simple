d762790faa102953da9bd90c39f274cfd8750d8a
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-09-02 17:52:17 -0400

    added printUsers

--- src/main/java/http/client/java/JavaHttpClientDemo.java
+++ src/main/java/http/client/java/JavaHttpClientDemo.java
@@ -1,20 +1,50 @@
 package http.client.java;
 
 import http.client.model.Page;
+import http.client.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 
 
 public class JavaHttpClientDemo {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(JavaHttpClientDemo.class);
+
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         synchronousRequest();
         asynchronousRequest();
+    }
+
+    public static void printUsers(final Page page) {
+
+        boolean printAll = false;
+        List<User> users = page.getData();
+        Iterator<User> i = users.iterator();
+
+        int threshold = 10;
+
+        while (i.hasNext()) {
+            User user  = (User) i.next();
+            if (user.getCommentCount().compareTo(Integer.valueOf(String.valueOf(threshold))) > 0) {
+                LOGGER.info(user.getUsername().toString() + " : " + user.getCommentCount().toString());
+            }
+        }
+
+        if (printAll) {
+            for (User user : users) {
+                LOGGER.info(user.getSubmissionCount().toString());
+            }
+        }
     }
 
     private static void asynchronousRequest() throws InterruptedException, ExecutionException {
@@ -37,7 +67,7 @@ public class JavaHttpClientDemo {
         var response = responseFuture.get();
 
         // the response:
-        System.out.println(response.body().get().getPage().toString());
+        printUsers(response.body().get());
     }
 
     private static void synchronousRequest() throws IOException, InterruptedException {
@@ -53,7 +83,7 @@ public class JavaHttpClientDemo {
         HttpResponse<Supplier<Page>> response = client.send(request, new JsonBodyHandler<>(Page.class));
 
         // the response:
-        System.out.println(response.body().get().getPage().toString());
+        printUsers(response.body().get());
     }
 
 }
