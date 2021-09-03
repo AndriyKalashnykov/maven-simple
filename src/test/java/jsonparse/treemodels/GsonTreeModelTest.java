b0241984298178c5cfe3ba48916ba3be9ea6be0e
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-09-03 12:05:52 -0400

    added more tests

---
+++ src/test/java/jsonparse/treemodels/GsonTreeModelTest.java
@@ -0,0 +1,19 @@
+package jsonparse.treemodels;
+
+import jsonparse.pathqueries.JsonPathExample;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class GsonTreeModelTest {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(GsonTreeModelTest.class);
+
+    @Test
+    public void mainTest() throws IOException {
+
+        GsonTreeModel.main(new String[] {});
+    }
+}
---
+++ src/test/java/jsonparse/treemodels/JacksonTreeModelTest.java
@@ -0,0 +1,18 @@
+package jsonparse.treemodels;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class JacksonTreeModelTest {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(JacksonTreeModelTest.class);
+
+    @Test
+    public void mainTest() throws IOException {
+
+        JacksonTreeModel.main(new String[] {});
+    }
+}
