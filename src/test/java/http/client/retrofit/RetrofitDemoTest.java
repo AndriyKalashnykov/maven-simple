97d96dbf428ef1d95056eeeb7aa603b2be8e96cc
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-09-03 11:16:29 -0400

    added more tests

---
+++ src/test/java/http/client/retrofit/RetrofitDemoTest.java
@@ -0,0 +1,18 @@
+package http.client.retrofit;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+
+public class RetrofitDemoTest {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(RetrofitDemoTest.class);
+
+    @Test
+    public void mainTest() throws ExecutionException, InterruptedException {
+
+        RetrofitDemo.main(new String[] {});
+    }
+}
