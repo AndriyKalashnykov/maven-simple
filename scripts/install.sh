d95751d40075ddd2249c58f2fb2c57a25156b591
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-09-08 19:36:38 -0400

    updated install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,11 +10,6 @@ cd $SCRIPT_DIR
 # ./install.sh 11.0.1.hs-adpt 3.8.1 no noq
 # 11.0.11.hs-adpt,11.0.10.hs-adpt,11.0.9.hs-adpt
 
-JAVA_VER=${1:-11.0.11.hs-adpt}
-MAVEN_VER=${2:-3.8.2}
-SET_JAVA_VER_DEFAULT=${3:-no}
-SET_MAVEN_VER_DEFAULT=${4:-no}
-
 export SDKMAN_DIR="$HOME/.sdkman"
 
 if [ -z ${SDKMAN_DIR+x} ] || [[ ! -d "$SDKMAN_DIR" ]] ; then
@@ -23,6 +18,21 @@ if [ -z ${SDKMAN_DIR+x} ] || [[ ! -d "$SDKMAN_DIR" ]] ; then
 else
   echo "Sdkman! already installed."
 fi
+# Bring 'sdk' function into scope
+source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+# get latest available sdkman java version
+LATEST_SDKMAN_JAVA_VER=$(sdk ls java | grep .hs-adpt | grep 11. -m1 | cut -c 62-)
+
+# sdk list maven | grep 3. | head -n 1
+
+JAVA_VER=${1:-$LATEST_SDKMAN_JAVA_VER}
+MAVEN_VER=${2:-3.8.2}
+SET_JAVA_VER_DEFAULT=${3:-no}
+SET_MAVEN_VER_DEFAULT=${4:-no}
+
+#echo $JAVA_VER
+#exit 1
 
 if [ ! -z "$JAVA_VER" ]; then
   echo "Installing Java: $JAVA_VER"
@@ -37,9 +47,6 @@ else
     echo "Specify Maven version"
     exit 1
 fi
-
-# Bring 'sdk' function into scope
-source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 # install $JAVA_VER if not installed
 if [ "$(sdk list java | grep -v "local only" | grep "$JAVA_VER" | grep -v "sdk install" | grep -v "installed" | wc -l)" == "1" ]; then
@@ -86,6 +93,39 @@ install_for_each() {
     install_java_using_sdk "$version"
   done
 }
+
+# Use SDKMAN to install something using a partial version match
+sdk_install() {
+    local install_type=$1
+    local requested_version=$2
+    local prefix=$3
+    local suffix="${4:-"\\s*"}"
+    local full_version_check=${5:-".*-[a-z]+"}
+    if [ "${requested_version}" = "none" ]; then return; fi
+    # Blank will install latest stable AdoptOpenJDK version
+    if [ "${requested_version}" = "lts" ] || [ "${requested_version}" = "default" ]; then
+         requested_version=""
+    elif echo "${requested_version}" | grep -oE "${full_version_check}" > /dev/null 2>&1; then
+        echo "${requested_version}"
+    else
+        local regex="${prefix}\\K[0-9]+\\.[0-9]+\\.[0-9]+${suffix}"
+        local version_list="$(. ${SDKMAN_DIR}/bin/sdkman-init.sh && sdk list ${install_type} 2>&1 | grep -oP "${regex}" | tr -d ' ' | sort -rV)"
+        if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ]; then
+            requested_version="$(echo "${version_list}" | head -n 1)"
+        else
+            set +e
+            requested_version="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
+            set -e
+        fi
+        if [ -z "${requested_version}" ] || ! echo "${version_list}" | grep "^${requested_version//./\\.}$" > /dev/null 2>&1; then
+            echo -e "Version $2 not found. Available versions:\n${version_list}" >&2
+            exit 1
+        fi
+    fi
+    "${SDKMAN_DIR}/bin/sdkman-init.sh && sdk install ${install_type} ${requested_version} && sdk flush archives && sdk flush temp"
+}
+
+#sdk_install maven ${MAVEN_VER} '\s\s' '\s\s' '^[0-9]+\.[0-9]+\.[0-9]+$'
 
 #versions_to_delete=$(sdk list java | grep "local only" | cut -c 62-)
 #java_versions_to_install=$(sdk list java | grep -v "local only" | grep "hs-adpt" | grep -v "sdk install" | grep -v "installed" | cut -c 62-)
