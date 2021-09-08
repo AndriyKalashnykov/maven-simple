657b1a8938676163962130a1a5093bf5f5d7470b
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-09-08 01:47:02 -0400

    updated install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,9 +7,10 @@ LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; c
 cd $SCRIPT_DIR
 
 # Install java/maven with sdkman
-# ./install.sh 11.0.11.hs-adpt 3.8.2 no no
+# ./install.sh 11.0.9.hs-adpt 3.8.1 no noq
+# 11.0.1.hs-adpt,11.0.10.hs-adpt,11.0.9.hs-adpt
 
-JAVA_VER=${1:-11.0.11.hs-adpt}
+JAVA_VER=${1:-11.0.9.hs-adpt}
 MAVEN_VER=${2:-3.8.1}
 SET_JAVA_VER_DEFAULT=${3:-no}
 SET_MAVEN_VER_DEFAULT=${4:-no}
@@ -17,7 +18,7 @@ SET_MAVEN_VER_DEFAULT=${4:-no}
 export SDKMAN_DIR="$HOME/.sdkman"
 
 if [ -z ${SDKMAN_DIR+x} ] || [[ ! -d "$SDKMAN_DIR" ]] ; then
-    iecho "sdkman not detected, installing it"
+    echo "sdkman not detected, installing it"
     curl -s "https://get.sdkman.io?rcupdate=false" | bash
 else
   echo "Sdkman! already installed."
@@ -50,6 +51,27 @@ fi
 if [ "$SET_MAVEN_VER_DEFAULT" == "yes" ]; then
   sdk default maven $MAVEN_VER
 fi
+
+delete_for_each() {
+  versions=$*;
+  for version in $versions
+  do
+    sdk uninstall java "$version"
+  done
+}
+
+install_for_each() {
+  versions=$*;
+  for version in $versions
+  do
+    install_java_using_sdk "$version"
+  done
+}
+
+versions_to_delete=$(sdk list java | grep "local only" | cut -c 62-)
+java_versions_to_install=$(sdk list java | grep -v "local only" | grep "hs-adpt" | grep -v "sdk install" | grep -v "installed" | cut -c 62-)
+#delete_for_each "$versions_to_delete"
+#install_for_each "$java_versions_to_install"
 
 sdk current
 echo $JAVA_HOME
